20db93d6a8bada0a2ca83436a6db39bb7b5526df
JuniorSilva1998 <75331746+JuniorSilva1998@users.noreply.github.com>
2021-02-04 03:19:48 +0000

    Add files via upload
    * Text Size is now implemented, and is based on the text's actual font size and font instead of being done manually. The result is that text looks nicer, and easier to change names.
    * Text now render in UTF-8. Day numbers in the calendar still render ASCII characters only.

--- code/main.c
+++ code/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
@@ -8,14 +7,14 @@
 
 #define SCREEN_WIDTH 720
 #define SCREEN_HEIGHT 480
-
-#define TRUE 1
-#define FALSE 0
+#define REFRESH_RATE 60.F
 
 SDL_Window *window = NULL;
 SDL_Color *color = NULL;
 SDL_Renderer *renderer = NULL;
-TTF_Font *font = NULL;
+TTF_Font *fontTitle = NULL;
+TTF_Font *fontHeader = NULL;
+TTF_Font *fontParagraph = NULL;
 
 struct Date {
     int year;
@@ -68,14 +67,22 @@ int main(int arg, char *argv[]) {
 		printf("Failed to initialize!\n");
         return -1;
 	} else {
-        font = TTF_OpenFont("Arial.ttf", 48);
-		if (!font) {
+        fontTitle = TTF_OpenFont("Arial.ttf", SCREEN_HEIGHT / 13);
+		if (!fontHeader) {
+			printf("Failed to load Arial font! SDL_ttf Error: %s\n", TTF_GetError());
+		}
+        fontHeader = TTF_OpenFont("Arial.ttf", SCREEN_HEIGHT / 20);
+		if (!fontHeader) {
+			printf("Failed to load Arial font! SDL_ttf Error: %s\n", TTF_GetError());
+		}
+        fontParagraph = TTF_OpenFont("Arial.ttf", SCREEN_HEIGHT / 30);
+		if (!fontParagraph) {
 			printf("Failed to load Arial font! SDL_ttf Error: %s\n", TTF_GetError());
 		}
         setLocal(1);
         getTodayDate();
         setDateToTodayDate();
-		int quit = FALSE;
+		int quit = 0;
         while (!quit) {
 			SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
 			SDL_RenderClear(renderer);
@@ -101,7 +108,7 @@ int main(int arg, char *argv[]) {
 			SDL_Event e;
             while (SDL_PollEvent(&e) != 0) {
                 if (e.type == SDL_QUIT) {
-					quit = TRUE;
+					quit = 1;
 				} else if (e.type == SDL_KEYDOWN) {
 					switch (e.key.keysym.sym) {
                         case SDLK_UP:
@@ -155,9 +162,8 @@ int init() {
 		} else {
 			/* Create renderer for window */
 			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-			if (!renderer)
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+			if (!renderer) {
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
 				return -1;
 			} else {
 				/* Initialize SDL_ttf */
@@ -174,8 +180,12 @@ int init() {
 
 void close() {
 	/* Free global font */
-	TTF_CloseFont(font);
-	font = NULL;
+    TTF_CloseFont(fontTitle);
+	TTF_CloseFont(fontHeader);
+    TTF_CloseFont(fontParagraph);
+    fontTitle = NULL;
+	fontHeader = NULL;
+	fontParagraph = NULL;
 
 	/* Destroy window */
 	SDL_DestroyRenderer(renderer);
@@ -217,15 +227,15 @@ void setLocal(int region) {
 int isLeapYear(int year) {
     if(year > 1582) {
         if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0) {
-            return TRUE;
+            return 1;
         } else {
-            return FALSE;
+            return 0;
         }
     } else {
         if (year % 4 == 0) {
-            return TRUE;
+            return 1;
         } else {
-            return FALSE;
+            return 0;
         }
     }
 }
@@ -376,55 +386,78 @@ void renderDayOfWeek(SDL_Surface* textSurface, SDL_Color color) {
     recDayOfWeek.h = (SCREEN_HEIGHT - 80) / 14;
 
     SDL_Rect recDayOfWeekText;
-    recDayOfWeekText.x = 45;
+    recDayOfWeekText.x = 40;
     recDayOfWeekText.y = 55;
-    recDayOfWeekText.w = (SCREEN_WIDTH - 80) / 8;
-    recDayOfWeekText.h = (SCREEN_HEIGHT - 80) / 20;
+    recDayOfWeekText.w = 0;
+    recDayOfWeekText.h = 0;
 
-    int dayOfWeek;
+    int dayOfWeek, textW, textH;
     for (dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
         if ((dayOfWeek == 0 && local.region != 2) || (dayOfWeek == 6 && local.region == 2)) {
 			SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
 			SDL_Color color = {0xFF, 0x00, 0x00};
             SDL_RenderDrawRect(renderer, &recDayOfWeek);
             if (local.region != 2) {
-                textSurface = TTF_RenderText_Solid(font, getDayName(dayOfWeek), color);
+                textSurface = TTF_RenderUTF8_Solid(fontParagraph, getDayName(dayOfWeek), color);
+                TTF_SizeUTF8(fontParagraph, getDayName(dayOfWeek), &textW, &textH);
             } else {
-                textSurface = TTF_RenderText_Solid(font, getDayName(dayOfWeek - 6), color);
+                textSurface = TTF_RenderUTF8_Solid(fontParagraph, getDayName(dayOfWeek - 6), color);
+                TTF_SizeUTF8(fontParagraph, getDayName(dayOfWeek - 6), &textW, &textH);
             }
+            recDayOfWeekText.x += ((SCREEN_WIDTH - 80) / 14) - (textW / 2);
+            recDayOfWeekText.w = textW;
+            recDayOfWeekText.h = textH;
+            
 		    SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 		    SDL_RenderCopy(renderer, dayName, NULL, &recDayOfWeekText);
 		    SDL_DestroyTexture(dayName);
 		    SDL_FreeSurface(textSurface);
 		    textSurface = NULL;
+
+            recDayOfWeekText.x -= ((SCREEN_WIDTH - 80) / 14) - (textW / 2);
         } else if ((dayOfWeek == 6 && local.region != 2) || (dayOfWeek == 5 && local.region == 2)) {
 			SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0xFF, 0xFF);
 			SDL_Color color = {0x00, 0xFF, 0xFF};
             SDL_RenderDrawRect(renderer, &recDayOfWeek);
             if (local.region != 2) {
-                textSurface = TTF_RenderText_Solid(font, getDayName(dayOfWeek), color);
+                textSurface = TTF_RenderUTF8_Solid(fontParagraph, getDayName(dayOfWeek), color);
+                TTF_SizeUTF8(fontParagraph, getDayName(dayOfWeek), &textW, &textH);
             } else {
-                textSurface = TTF_RenderText_Solid(font, getDayName(dayOfWeek + 1), color);
+                textSurface = TTF_RenderUTF8_Solid(fontParagraph, getDayName(dayOfWeek + 1), color);
+                TTF_SizeUTF8(fontParagraph, getDayName(dayOfWeek + 1), &textW, &textH);
+	            
             }
+            recDayOfWeekText.x += ((SCREEN_WIDTH - 80) / 14) - (textW / 2);
+            recDayOfWeekText.w = textW;
+            recDayOfWeekText.h = textH;
+
 		    SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 		    SDL_RenderCopy(renderer, dayName, NULL, &recDayOfWeekText);
 		    SDL_DestroyTexture(dayName);
 		    SDL_FreeSurface(textSurface);
 		    textSurface = NULL;
+            recDayOfWeekText.x -= ((SCREEN_WIDTH - 80) / 14) - (textW / 2);
         } else {
             SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
             SDL_Color color = {0xFF, 0xFF, 0xFF};
             SDL_RenderDrawRect(renderer, &recDayOfWeek);
             if (local.region != 2) {
-                textSurface = TTF_RenderText_Solid(font, getDayName(dayOfWeek), color);
+                textSurface = TTF_RenderUTF8_Solid(fontParagraph, getDayName(dayOfWeek), color);
+                TTF_SizeUTF8(fontParagraph, getDayName(dayOfWeek), &textW, &textH);
             } else {
-                textSurface = TTF_RenderText_Solid(font, getDayName(dayOfWeek + 1), color);
+                textSurface = TTF_RenderUTF8_Solid(fontParagraph, getDayName(dayOfWeek + 1), color);
+                TTF_SizeUTF8(fontParagraph, getDayName(dayOfWeek + 1), &textW, &textH);
             }
+            recDayOfWeekText.x += ((SCREEN_WIDTH - 80) / 14) - (textW / 2);
+            recDayOfWeekText.w = textW;
+            recDayOfWeekText.h = textH;
+
 		    SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 		    SDL_RenderCopy(renderer, dayName, NULL, &recDayOfWeekText);
 		    SDL_DestroyTexture(dayName);
 		    SDL_FreeSurface(textSurface);
 		    textSurface = NULL;
+            recDayOfWeekText.x -= ((SCREEN_WIDTH - 80) / 14) - (textW / 2);
         }
         recDayOfWeek.x += (SCREEN_WIDTH - 80) / 7;
         recDayOfWeekText.x += (SCREEN_WIDTH - 80) / 7;
@@ -432,19 +465,15 @@ void renderDayOfWeek(SDL_Surface* textSurface, SDL_Color color) {
 }
 
 void renderYear(SDL_Surface* textSurface, SDL_Color color) {
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-
-    char yearStr[4];
+    char yearStr[5]; /* The reason yearStr is 5 elements long is due to glitches if 4 was the maximum. */
     itoa(date.year, yearStr, 10);
 
-    textSurface = TTF_RenderText_Solid(font, yearStr, color);
+    textSurface = TTF_RenderUTF8_Solid(fontHeader, yearStr, color);
 	SDL_Texture* yearName = SDL_CreateTextureFromSurface(renderer, textSurface);
 
-    SDL_Rect recYear;
-    recYear.x = ((SCREEN_WIDTH - 80) / 6) * 4 + 40;
-    recYear.y = 20;
-    recYear.w = (SCREEN_WIDTH - 80) / 6;
-    recYear.h = (SCREEN_HEIGHT - 80) / 14;
+    int textW, textH;
+	TTF_SizeUTF8(fontHeader, yearStr, &textW, &textH);
+	SDL_Rect recYear = {((SCREEN_WIDTH - 80) * 2 / 3) + 40 - (textW / 2), 20, textW, textH};
 
 	SDL_RenderCopy(renderer, yearName, NULL, &recYear);
 	SDL_DestroyTexture(yearName);
@@ -453,14 +482,13 @@ void renderYear(SDL_Surface* textSurface, SDL_Color color) {
 }
 
 void renderMonth(SDL_Surface* textSurface, SDL_Color color) {
-    SDL_Rect recMonth;
-    recMonth.x = (SCREEN_WIDTH - 80) / 4 + 40;
-    recMonth.y = 20;
-    recMonth.w = ((SCREEN_WIDTH - 80) / 32) * strlen(getMonthName(date.month));
-    recMonth.h = (SCREEN_HEIGHT - 80) / 14;
-
-    textSurface = TTF_RenderText_Solid(font, getMonthName(date.month), color);
+    textSurface = TTF_RenderUTF8_Solid(fontHeader, getMonthName(date.month), color);
 	SDL_Texture* monthName = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    int textW, textH;
+	TTF_SizeUTF8(fontHeader, getMonthName(date.month), &textW, &textH);
+	SDL_Rect recMonth = {((SCREEN_WIDTH - 80) / 3) + 40 - (textW / 2), 20, textW, textH};
+
 	SDL_RenderCopy(renderer, monthName, NULL, &recMonth);
 	SDL_DestroyTexture(monthName);
 	SDL_FreeSurface(textSurface);
@@ -475,17 +503,17 @@ void renderDate() {
     recDayOfMonth.h = (SCREEN_HEIGHT - 80) / 8;
 
     SDL_Rect recDayOfMonthText;
-    recDayOfMonthText.x = 70;
+    recDayOfMonthText.x = 40 + ((SCREEN_WIDTH - 80) / 7) / 2;
     recDayOfMonthText.y = 85;
-    recDayOfMonthText.w = ((SCREEN_WIDTH - 80) / 16);
-    recDayOfMonthText.h = (SCREEN_HEIGHT - 80) / 10;
+    recDayOfMonthText.w = 0;
+    recDayOfMonthText.h = 0;
 
     SDL_Surface* textSurface;
 	SDL_Color color = {0xFF, 0xFF, 0xFF};
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
 	int daysInMonth = getNumberOfDaysInMonth(date.year, date.month);
-	int day;
+	int day, textW, textH;
     char dayStr[2];
     int dayOfWeek = getDayNumber(date.year, date.month, 1);
 
@@ -510,58 +538,78 @@ void renderDate() {
             SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
 			SDL_Color color = {0x00, 0xFF, 0x00};
 			SDL_RenderDrawRect(renderer, &recDayOfMonth);
-			textSurface = TTF_RenderText_Solid(font, dayStr, color);
+
+			textSurface = TTF_RenderText_Solid(fontTitle, dayStr, color);
+            TTF_SizeUTF8(fontTitle, dayStr, &textW, &textH);
+            recDayOfMonthText.x -= (textW / 2);
+            recDayOfMonthText.w = textW;
+            recDayOfMonthText.h = textH;
+
 			SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 			SDL_RenderCopy(renderer, dayName, NULL, &recDayOfMonthText);
 			SDL_DestroyTexture(dayName);
 			SDL_FreeSurface(textSurface);
 			textSurface = NULL;
+            recDayOfMonthText.x += (textW / 2);
         } else if (dayOfWeek == 0) {
 			SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
 			SDL_Color color = {0xFF, 0x00, 0x00};
 			SDL_RenderDrawRect(renderer, &recDayOfMonth);
-			textSurface = TTF_RenderText_Solid(font, dayStr, color);
+
+			textSurface = TTF_RenderText_Solid(fontTitle, dayStr, color);
+            TTF_SizeUTF8(fontTitle, dayStr, &textW, &textH);
+            recDayOfMonthText.x -= (textW / 2);
+            recDayOfMonthText.w = textW;
+            recDayOfMonthText.h = textH;
+
 			SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 			SDL_RenderCopy(renderer, dayName, NULL, &recDayOfMonthText);
 			SDL_DestroyTexture(dayName);
 			SDL_FreeSurface(textSurface);
 			textSurface = NULL;
+            recDayOfMonthText.x += (textW / 2);
 		} else if (dayOfWeek == 6) {
 			SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0xFF, 0xFF);
 			SDL_Color color = {0x00, 0xFF, 0xFF};
 			SDL_RenderDrawRect(renderer, &recDayOfMonth);
-			textSurface = TTF_RenderText_Solid(font, dayStr, color);
+
+			textSurface = TTF_RenderText_Solid(fontTitle, dayStr, color);
+            TTF_SizeUTF8(fontTitle, dayStr, &textW, &textH);
+            recDayOfMonthText.x -= (textW / 2);
+            recDayOfMonthText.w = textW;
+            recDayOfMonthText.h = textH;
+
 			SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 			SDL_RenderCopy(renderer, dayName, NULL, &recDayOfMonthText);
 			SDL_DestroyTexture(dayName);
 			SDL_FreeSurface(textSurface);
 			textSurface = NULL;
+            recDayOfMonthText.x += (textW / 2);
 		} else {
 			SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 			SDL_Color color = {0xFF, 0xFF, 0xFF};
 			SDL_RenderDrawRect(renderer, &recDayOfMonth);
-			textSurface = TTF_RenderText_Solid(font, dayStr, color);
+
+			textSurface = TTF_RenderText_Solid(fontTitle, dayStr, color);
+            TTF_SizeUTF8(fontTitle, dayStr, &textW, &textH);
+            recDayOfMonthText.x -= (textW / 2);
+            recDayOfMonthText.w = textW;
+            recDayOfMonthText.h = textH;
+
 			SDL_Texture* dayName = SDL_CreateTextureFromSurface(renderer, textSurface);
 			SDL_RenderCopy(renderer, dayName, NULL, &recDayOfMonthText);
 			SDL_DestroyTexture(dayName);
 			SDL_FreeSurface(textSurface);
+            recDayOfMonthText.x += (textW / 2);
 		}
 		recDayOfMonth.x += (SCREEN_WIDTH - 80) / 7;
         recDayOfMonthText.x += (SCREEN_WIDTH - 80) / 7;
-        if (day == 8 || (date.year == 1582 && date.month == 10 && day == 3)) {
-            recDayOfMonthText.x -= 25;
-            recDayOfMonthText.w *= 2;
-        }
         if (local.region != 2) {
             if (dayOfWeek == 6) {
 			    dayOfWeek = 0;
 			    recDayOfMonth.x = 40;
-                if (day < 8) {
-                    recDayOfMonthText.x = 70;
-                } else {
-                    recDayOfMonthText.x = 45;
-                }
 			    recDayOfMonth.y += (SCREEN_HEIGHT - 80) / 8;
+                recDayOfMonthText.x = 40 + ((SCREEN_WIDTH - 80) / 7) / 2;
                 recDayOfMonthText.y += (SCREEN_HEIGHT - 80) / 8;
 		    } else {
 			    dayOfWeek++;
@@ -571,12 +619,8 @@ void renderDate() {
 			    dayOfWeek = 0;
             } else if (dayOfWeek == 0) {
 			    recDayOfMonth.x = 40;
-                if (day < 8) {
-                    recDayOfMonthText.x = 70;
-                } else {
-                    recDayOfMonthText.x = 45;
-                }
 			    recDayOfMonth.y += (SCREEN_HEIGHT - 80) / 8;
+                recDayOfMonthText.x = 40 + ((SCREEN_WIDTH - 80) / 7) / 2;
                 recDayOfMonthText.y += (SCREEN_HEIGHT - 80) / 8;
                 dayOfWeek++;
 		    } else {
@@ -587,25 +631,10 @@ void renderDate() {
 }
 
 void renderTime(SDL_Surface* textSurface, SDL_Color color) {
-    SDL_Rect recDate;
-    recDate.x = 40;
-    recDate.y = SCREEN_HEIGHT - 80;
-    recDate.w = (SCREEN_WIDTH - 80) / 5;
-    recDate.h = (SCREEN_HEIGHT - 80) / 16;
-    
-    SDL_Rect recTime;
-    recTime.x = (SCREEN_WIDTH - 80) / 4 + 40;
-    recTime.y = SCREEN_HEIGHT - 80;
-    recTime.w = (SCREEN_WIDTH - 80) / 5;
-    recTime.h = (SCREEN_HEIGHT - 80) / 16;
-
-    SDL_Rect recLocale;
-    recLocale.x = (SCREEN_WIDTH - 80) / 2 + 40;
-    recLocale.y = SCREEN_HEIGHT - 80;
-    recLocale.w = (SCREEN_WIDTH - 80) / 2;
-    recLocale.h = (SCREEN_HEIGHT - 80) / 16;
-
-    int locale = local.region; /* International = 0, North America = 1, Europe = 2 */
+    /*
+        International = 0, North America = 1, Europe = 2
+    */
+    int locale = local.region;
 
     char date[25] = "";
     char time[25] = "";
@@ -675,8 +704,13 @@ void renderTime(SDL_Surface* textSurface, SDL_Color color) {
         strcat(date, dayStr);
     }
     
-    textSurface = TTF_RenderText_Solid(font, date, color);
+    textSurface = TTF_RenderUTF8_Solid(fontHeader, date, color);
 	SDL_Texture* displayDate = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    int textW, textH;
+	TTF_SizeUTF8(fontHeader, date, &textW, &textH);
+	SDL_Rect recDate = {100 - (textW / 2), (SCREEN_HEIGHT - 80), textW, textH};
+
 	SDL_RenderCopy(renderer, displayDate, NULL, &recDate);
 	SDL_DestroyTexture(displayDate);
 	SDL_FreeSurface(textSurface);
@@ -728,15 +762,23 @@ void renderTime(SDL_Surface* textSurface, SDL_Color color) {
         }
     }
 
-    textSurface = TTF_RenderText_Solid(font, time, color);
+    textSurface = TTF_RenderUTF8_Solid(fontHeader, time, color);
 	SDL_Texture* displayTime = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+	TTF_SizeUTF8(fontHeader, time, &textW, &textH);
+	SDL_Rect recTime = {(SCREEN_WIDTH - 80) / 4 + 100 - (textW / 2), (SCREEN_HEIGHT - 80), textW, textH};
+
 	SDL_RenderCopy(renderer, displayTime, NULL, &recTime);
 	SDL_DestroyTexture(displayTime);
 	SDL_FreeSurface(textSurface);
 	textSurface = NULL;
 
-    textSurface = TTF_RenderText_Solid(font, localeText, color);
+    textSurface = TTF_RenderUTF8_Solid(fontHeader, localeText, color);
 	SDL_Texture* displayLocale = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+	TTF_SizeUTF8(fontHeader, localeText, &textW, &textH);
+	SDL_Rect recLocale = {(SCREEN_WIDTH - 80) / 2 + 40, (SCREEN_HEIGHT - 80), textW, textH};
+
 	SDL_RenderCopy(renderer, displayLocale, NULL, &recLocale);
 	SDL_DestroyTexture(displayLocale);
 	SDL_FreeSurface(textSurface);
